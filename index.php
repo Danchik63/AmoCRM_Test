9191a840a43110dbbd6126d45a79dc98bf3ecc9c
Danchik63 <danchik55663@gmail.com>
2025-12-29 01:33:45 +0400

    Поправлена работа автозагрузчика

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/Controllers/FormController.php';
-require_once __DIR__ . '/DTOs/FormDTO.php';
-require_once __DIR__ . '/Services/FormService.php';
 use Controllers\FormController;
 
 $method = $_SERVER['REQUEST_METHOD'];
